8cd7eea1ecf388031e7852217ec46ad462c7b284
Jose Mora <jdevmora@gmail.com>
2015-11-07 03:33:17 -0600

    Creación del proyecto

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Proyecto #1
-Primer proyecto, Fundamentos de programacion
+# Hotel Lounge
+Primer proyecto, Fundamentos de programación
+'''
+José David Mora Loría
+Oscar Mauricio Gil
+Melvin Arce Rodriguez
+'''
+Tercer trimestre, 2014. Ulacit
---
+++ src/hotellounge/HotelLounge.java
@@ -0,0 +1,23 @@
+/*
+ * HotelLounge
+ * Jos\u00e9 David Mora Lor\u00ed
+ * Oscar Mauricio Gil
+ * Melvin Arce Rodriguez
+ * Nov 7, 2015
+ */
+package hotellounge;
+
+/**
+ *
+ * @author jmora
+ */
+public class HotelLounge {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
