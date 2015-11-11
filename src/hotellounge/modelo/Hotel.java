0ee7ef4c236061549bd3cc37b67bba37c7d17a7c
Jose Mora <jdevmora@gmail.com>
2015-11-11 12:41:43 -0600

    -Creacion clase Hotel y paquete modelo.

---
+++ src/hotellounge/modelo/Hotel.java
@@ -0,0 +1,17 @@
+/*
+ * HotelLounge
+ * José David Mora Loría
+ * Oscar Mauricio Gil
+ * Melvin Arce Rodriguez
+ * Marvin Andrei Sandi Brenes
+ * Nov 11, 2015
+ */
+package hotellounge.modelo;
+
+/**
+ *
+ * @author jmora
+ */
+public class Hotel {
+    
+}
