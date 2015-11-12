4406af0c013ac4f95e1367dc836dc48014e40cd9
Jokome <cocho_1756@hotmail.com>
2015-11-12 12:22:17 -0600

    - Variables declaradas.
    En la clase Reservación no se si poner las variables cliente, código de
    habitación y actividades. porque no si si después uno las llama desde la
    clases correspondidas.

---
+++ src/hotellounge/modelo/Reservacion.java
@@ -0,0 +1,17 @@
+/*
+ * HotelLounge
+ * José David Mora Loría
+ * Oscar Mauricio Gil
+ * Melvin Arce Rodriguez
+ * Marvin Andrei Sandi Brenes
+ * 12-nov-2015
+ */
+package hotellounge.modelo;
+
+/**
+ *
+ * @author Cocho
+ */
+public class Reservacion {
+    
+}
